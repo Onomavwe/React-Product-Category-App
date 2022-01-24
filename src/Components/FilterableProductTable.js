d5934e7c90058c38552342726ebacea835f75ec1
Ochuko Onomavwe <ochuks01@gmail.com>
2022-01-24 23:42:00 +0100

    The React App Demo

---
+++ src/Components/FilterableProductTable.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import ProductTable from './ProductTable';
+import SearchBar from './SearchBar';
+
+export default function FilterableProductTable(props) {
+    const { products } = props;
+
+    const [filterText, setFilterText] = useState('')
+    const handleFilterTextChange = (value) => [
+        setFilterText(value)
+    ]
+
+    const [inStockOnly, setInStockOnly] = useState(false)
+
+    const handleInStockChange = (value) => {
+        setInStockOnly(value)
+    }
+
+    return(
+        <div>
+           <SearchBar 
+           filterText={filterText}
+           onFilterTextChange={handleFilterTextChange}
+           inStockOnly={inStockOnly}
+           onInStockChange={handleInStockChange}
+           />
+           <ProductTable 
+           inStockOnly={inStockOnly}
+           products={products} 
+           filterText={filterText}
+           /> 
+        </div>
+    );    
+}
